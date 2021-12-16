89d293fc0f7ab08162c4f00d2f9ad75028b5e6a5
JOgden99 <josh.ogden.1999@gmail.com>
2021-12-16 13:13:54 -0700

    Begin Function for creating plot

---
+++ tethysapp/regional_bias_correction/public/js/data_requests.js
@@ -0,0 +1,41 @@
+let request = {
+    reachid: 3001207,
+    lat: 100.0,
+    lon: 45.0
+}
+
+let response = {
+    datetime: ['2021-06-01 00:00:00','2021-06-01 00:03:00'],
+    original_flow: [100.0, 110.0],
+    bias_corrected_flow: [101.0, 112.0],
+    reachid: 3001207,
+    lat: 100.0,
+    lon: 45.0
+}
+
+let layout = {
+  title: `Original vs. Bias Corrected Flow for ReachID: ${response['reachid']}`,
+  xaxis: {
+    title: 'DateTime',
+    showgrid: true
+  },
+  yaxis: {
+    title: 'Flow Rate m^3/s',
+    showgrid: true
+  }
+};
+
+const MakePlot = (response) => {
+    let original_trace = {
+        x: response.datetime,
+        y: response.original_flow,
+        mode: 'lines',
+        name: 'Original Forecast'
+    }
+    let corrected_trace = {
+        x: response.datetime,
+        y: response.bias_corrected_flow,
+        mode: 'lines',
+        name: 'Bias Corrected Forecast'
+    }
+}
